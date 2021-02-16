3609148b3accd0e4c2ae4419cb77df987067a481
Saurav Adhikari <sauravads123@gmail.com>
2021-02-16 16:44:22 +0545

    feat : added counting down

---
+++ src/components/Countdown/index.js
@@ -0,0 +1,42 @@
+import React,{useEffect, useState} from "react";
+import {Text,StyleSheet} from "react-native";
+import {fontSizes,spacing} from "../../utils/sizes"
+import {colors} from "../../utils/colors"
+
+const minutesToMilli = (min)=>min*1000 *60;
+const formatTime= (time)=>time<10?`0${time}`:time;
+
+export const Countdown=({
+    minutes=10,
+    isPaused,
+})=>{
+    const countDown = ()=>{
+        setMillis(time =>{
+            if(time===0)
+                return time;
+            const timeLeft=time-1000;
+            return timeLeft;
+        })
+    }
+    const interval= React.useRef(null);
+    const [millis,setMillis]=useState(minutesToMilli(minutes));
+
+    useEffect(()=>{
+        interval.current=setInterval(countDown,1000)
+        return ()=>clearInterval(interval.current)
+    },[])
+
+    const minute = Math.floor(millis/1000/60)%60;
+    const seconds = Math.floor(millis/1000)%60;
+    return <Text style={styles.text}>{formatTime(minute)}:{formatTime(seconds)}</Text>
+}
+
+const styles= StyleSheet.create({
+    text: {
+        fontSize: fontSizes.xxxl,
+        fontWeight: 'bold',
+        color: colors.white,
+        padding: spacing.lg,
+        backgroundColor: colors.primary,
+    },
+})
--- src/features/Timer/index.js
+++ src/features/Timer/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import {StyleSheet, Text, View} from "react-native";
 import { colors } from "../../utils/colors";
 import { spacing } from "../../utils/sizes";
+import {Countdown} from "../../components/Countdown"
 
 export const Timer = ({focusSubject})=>{
 
     return <View styles={styles.container}>
-        <View>
-            <Text style={styles.title}>Focusing on</Text>
+          <View style={styles.countdown}>
+            <Countdown/>
+          </View>
+          <View style={{ paddingTop: spacing.xxxl }}>
+            <Text style={styles.title}>Focusing on:</Text>
             <Text style={styles.task}>{focusSubject}</Text>
+          </View>
         </View>
-    </View>
 }
 
 const styles = StyleSheet.create({
@@ -26,5 +30,10 @@ const styles = StyleSheet.create({
         color:colors.black,
         fontWeight:"bold",
         textAlign:"center"
+    },
+    countdown:{
+        flex:.5,
+        alignItems:"center",
+        justifyContent:"center"
     }
 })
