d506052794e8865d0f17f5e4512420171cdd28b6
Saurav Adhikari <sauravads123@gmail.com>
2021-02-16 13:53:48 +0545

    feat : added focus component

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {Focus} from "./src/features/focus"
+
 export default function App() {
+  // const [focusSubject,setFocusSubjet]=useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Focus/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1, 
+    backgroundColor: '#252250',
   },
 });
